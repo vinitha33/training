9ac47710eab2ecc25168adf28af40f0338bcffb4
vinitha33 <61783997+vinitha33@users.noreply.github.com>
2020-05-15 20:30:20 +0530

    Add files via upload

---
+++ Hands-on/PythonSets/ProblemSet01/p9.py
@@ -0,0 +1,15 @@
+width = 17
+height = 12.0
+delimiter = "."
+
+a = width/2
+b = width/2.0
+c = height/3
+d = 1 + 2 * 5
+e = delimiter * 5
+
+print("a = ", a, " Type = ", type(a))
+print("b = ", b, " Type = ", type(b))
+print("c = ", c, " Type = ", type(c))
+print("d = ", d, " Type = ", type(d))
+print("e = ", e, " Type = ", type(e))
