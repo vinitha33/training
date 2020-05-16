ba0f7009c79214f6c61c46866939360e444bf33e
vinitha33 <61783997+vinitha33@users.noreply.github.com>
2020-05-16 20:04:19 +0530

    Update p1.py

--- Hands-on/PythonSets/ProblemSet02/p1.py
+++ Hands-on/PythonSets/ProblemSet02/p1.py
@@ -1,10 +1,15 @@
-def GCD(a,b):
-    if b<=a:
-        rem = a%b
-    if rem == 0:
+def gcd(a,b):
+    if a == 0:
         return "GCD is {}".format(b)
-    else:
-        return GCD(b,rem)
+    elif b == 0:
+        return "GCD is {}".format(a)
+    elif a == b:
+        return "GCD is {}".format(b)
+    elif a > b:
+        return gcd(a-b,b)
+    return gcd(a,b-a)
 
-result = GCD(25,20)
-print(result)
+num1 = int(input("Enter first no:"))
+num2 = int(input("Enter second no:"))
+
+print(gcd(num1,num2))
