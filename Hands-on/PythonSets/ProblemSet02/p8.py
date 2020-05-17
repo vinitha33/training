ddc72dd9ea856399f6c6e63c7819f2225794f1bd
vinitha33 <61783997+vinitha33@users.noreply.github.com>
2020-05-17 20:49:58 +0530

    Update p8.py

--- Hands-on/PythonSets/ProblemSet02/p8.py
+++ Hands-on/PythonSets/ProblemSet02/p8.py
@@ -1,22 +1,10 @@
-num1 = int(input("Enter how many numbers you want to enter:"))
-a = []
-b = []
-e = []
-for i in range(0,num1):
-    x = int(input())
-    a.append(x)
-
-for i in range(0,num1):
-    x = int(input())
-    b.append(x)
-
-try:
-    for i in range(len(b)):
-        if a[i]/b[i] != 0:
-            e.append(a[i]/b[i])
+def eval_loop():
+    while True:
+        a = input()
+        if a != "done":
+            s = eval(a)
+            print(s)
         else:
-            raise Exception()
-    print(e)
+            print(s)
 
-except:
-    print("An exception occured")
+print(eval_loop())
