d6d9780e50a7256dba7218b3e7ecc93ee9d57fdb
vinitha33 <61783997+vinitha33@users.noreply.github.com>
2020-05-17 20:40:07 +0530

    Update p7.py

--- Hands-on/PythonSets/ProblemSet02/p7.py
+++ Hands-on/PythonSets/ProblemSet02/p7.py
@@ -1,16 +1,17 @@
-def ispalindrome(s):
-    x = "abcdefghijklmnopqrstuvwxyz"
-    y = s.lower()
-    s1 = ""
-    for i in y:
-        if i in x:
-            s1 = s1 + i
-    print(s1)
+def isPalindrome(s):
+    if len(s) <= 1:
+        return True
+    s2 = ""
     s2 = s1[::-1]
-    print(s2)
-    if s1 in s2:
-        print("Palindrome")
+    if s1 == s2:
+        return True
     else:
-        print("Not Palindrome")
-s = input()
-ispalindrome(s)
+        return False
+
+str1 = input("Enter the string:")
+s1 = ""
+for i in str1:
+    if i.isalpha():
+        s1 += i
+s1 = s1.lower()
+print(isPalindrome(s1))
