d4a9088cc3db28b629291fa282e4e1d8e8b8bfa7
vinitha33 <61783997+vinitha33@users.noreply.github.com>
2020-05-22 12:30:25 +0530

    Update p1.py

--- Hands-on/PythonSets/ProblemSet03/p1.py
+++ Hands-on/PythonSets/ProblemSet03/p1.py
@@ -1,7 +1,8 @@
-def is_palindrome(string):
-    cp_string=string[::-1]
-    if cp_string==string:
+def is_Palindrome(str1):
+    str2 = str1[::-1]
+    if str1.lower() == str2.lower():
         return True
     else:
         return False
-print(is_palindrome(str(input())))
+
+print(is_Palindrome(input("Enter the string:")))
